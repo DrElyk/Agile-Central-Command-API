5015979df2e082dd5c024c8f1ccf247529c83663
LeeWildes <leeawildes@knights.ucf.edu>
2019-02-25 12:58:14 -0500

    Testing elasticache

--- agilecommandcentral/settings.py
+++ agilecommandcentral/settings.py
@@ -166,7 +166,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": ["redis://(dacc.tovdvi.0001.use2.cache.amazonaws.com
+, 6379)"],
         },
     },
 }
